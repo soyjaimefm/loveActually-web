8ced20886ed804276675db8735db1c9c9a183ffd
Jaime Fernández Moreno <jaimefrm93@gmail.com>
2024-06-05 18:25:12 +0200

    Updated texts

--- src/config/text.js
+++ src/config/text.js
@@ -5,20 +5,26 @@ const text = {
   section1: {
     feature1: {
       title: "PERSONALIZADA",
-      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus velit aliquam eos blanditiis unde magni nostrum. Doloribus modi architecto dolor nulla dicta necessitatibus ab at nobis quas veniam, nostrum ea!"
+      text: "En LOVE ACTUALLY entendemos que cada pareja y cada historia es única. Por eso, la web de su boda también debe serlo. Nos dedicamos a crear diseños completamente personalizados que reflejen vuestra forma de ser. Desde las formas y colores hasta los detalles más pequeños, cada elemento de la web estará hecho a medida para vosotros."
     },
     feature2: {
       title: "FUNCIONAL",
-      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus velit aliquam eos blanditiis unde magni nostrum. Doloribus modi architecto dolor nulla dicta necessitatibus ab at nobis quas veniam, nostrum ea!"
+      text: "Vuestra web destacará por su diseño, pero sobre todo será muy práctica, intuitiva y funcional. Queremos ayudarte a organizar vuestra boda, por eso integramos herramientas para gestionar invitados, añadimos mapas, horarios detallados… Infórmate de todos los servicios que podemos incluir en la web para mantener todo bajo control."
     },
     feature3: {
       title: "EMOCIONANTE",
-      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus velit aliquam eos blanditiis unde magni nostrum. Doloribus modi architecto dolor nulla dicta necessitatibus ab at nobis quas veniam, nostrum ea!"
+      text: "Disfrutamos del proceso de crear y eso se nota. Las bodas son celebraciones de amor y deben disfrutarse desde que preparas todo. Nos dedicamos a capturar y transmitir emociones como la ilusión en cada web que diseñamos. A través de un diseño cuidado y detalles personalizados, los invitados podrán sentir la magia de vuestro gran día."
     }
   },
   section2: {
     title: "¿Por qué es importante tener una web de vuestra boda?",
-    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus velit aliquam eos blanditiis unde magni nostrum. Doloribus modi architecto dolor nulla dicta necessitatibus ab at nobis quas veniam, nostrum ea!"
+    text: `Contar con la ayuda de alguien para crear la web de vuestra boda es esencial hoy en día por muchas razones. Lo primero y más importante, es que puedes centralizar toda la información importante de vuestro gran día en un solo lugar. ¿Te imaginas contestar a muchas de las preguntas que no paran de haceros con un: “está todo en la web”? :) Desde la invitación digital, los detalles del evento, las confirmaciones de asistencia, la lista de regalos si tenéis… Además, podéis personalizarla con vuestra historia, fotos y detalles únicos. Así haréis que todos empiecen a sentir la emoción del gran día.
+    <br>
+    <br>
+    Se trata de la manera más moderna y ecológica de compartir vuestra celebración y facilita la comunicación de cualquier actualización de última hora, asegurando así que todos puedan estar al tanto y no haya confusiones.
+    <br>
+    <br>
+    Con una web de vuestra boda, vuestros invitados pueden acceder a la información que necesitan en cualquier momento y desde cualquier lugar, lo que es perfecto para aquellos que viajan o tienen agendas imposibles. Tener una web de bodas no solo simplifica la planificación y organización, sino que añade un toque personal y especial a vuestra celebración, asegurando que todo salga perfecto y que vuestros seres queridos se sientan parte de este maravilloso momento de vuestras vidas.`
   }
 }
 
