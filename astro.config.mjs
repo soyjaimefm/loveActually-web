47dedbe2573d6ee9500896f21357b1865ec49603
Jaime Fernández Moreno <jaimefrm93@gmail.com>
2024-06-05 11:04:34 +0200

    Fixed analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind()],
   output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true }
-  }),
-  integrations: [tailwind()]
+  })
 });
