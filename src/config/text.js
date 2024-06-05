50ee5267db706ba8147790cb071a971606f34433
Jaime Fernández Moreno <jaimefrm93@gmail.com>
2024-06-05 10:37:56 +0200

    First home version

---
+++ src/config/text.js
@@ -0,0 +1,25 @@
+const text = {
+  heroSection: {
+    title: "Creamos la web de vuestra boda a medida"
+  },
+  section1: {
+    feature1: {
+      title: "PERSONALIZADA",
+      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus velit aliquam eos blanditiis unde magni nostrum. Doloribus modi architecto dolor nulla dicta necessitatibus ab at nobis quas veniam, nostrum ea!"
+    },
+    feature2: {
+      title: "FUNCIONAL",
+      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus velit aliquam eos blanditiis unde magni nostrum. Doloribus modi architecto dolor nulla dicta necessitatibus ab at nobis quas veniam, nostrum ea!"
+    },
+    feature3: {
+      title: "EMOCIONANTE",
+      text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus velit aliquam eos blanditiis unde magni nostrum. Doloribus modi architecto dolor nulla dicta necessitatibus ab at nobis quas veniam, nostrum ea!"
+    }
+  },
+  section2: {
+    title: "¿Por qué es importante tener una web de vuestra boda?",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus velit aliquam eos blanditiis unde magni nostrum. Doloribus modi architecto dolor nulla dicta necessitatibus ab at nobis quas veniam, nostrum ea!"
+  }
+}
+
+export default text;
---
+++ tailwind.config.mjs
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        softGreen: '#E8EBE0',
+        primaryGreen: '#7B7C4E',
+        darkGreen: '#41461B',
+        backGreen: '#7A7C4D',
+        beige: '#FCFCFC',
+        lavender: '#783D82',
+
+        /** Love actually */
+        laBackground: '#F3D5BE',
+        laBeige: '#F8EDE4',
+        laRed: '#E9523A',
+        laGreen: '#83712A',
+        laSoftGreen: '#B9B57C',
+        laSoftBlue: '#D5EBF8',
+        laOrange: '#EF9560'
+      },
+      grayscale: {
+        50: '50%'
+      },
+
+      fontFamily: {
+        theSeasons: 'TheSeasons',
+        alNevrada: 'AlNevrada',
+        roxboroughCf: 'RoxboroughCF'
+      }
+    },
+  },
+  plugins: [],
+}
