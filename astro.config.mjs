2bb360bc1583ab1263dccd2400a80fb9b54453f6
Jaime Fernández Moreno <jaimefrm93@gmail.com>
2024-06-07 18:32:16 +0200

    Added instagram and tiktok logos fixed 404 in contact page Added sitemap and robots.txt generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
+  site: 'https://www.loveactually.es/',
   output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: {
+      enabled: true
+    }
   })
 });
