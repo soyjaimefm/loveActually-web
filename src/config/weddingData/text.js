10d4477690d8f5472149a8a4a364528b8e93d41f
Jaime Fernández Moreno <jaimefrm93@gmail.com>
2024-06-29 11:13:11 +0200

    New metadata

---
+++ src/config/weddingData/text.js
@@ -0,0 +1,25 @@
+const text = {
+  heroSection: {
+    title: "Creamos la web de vuestra boda a medida"
+  },
+  section1: {
+    feature1: {
+      title: "",
+      text: ""
+    },
+    feature2: {
+      title: "",
+      text: ""
+    },
+    feature3: {
+      title: "",
+      text: ""
+    }
+  },
+  section2: {
+    title: "",
+    text: ``
+  }
+}
+
+export default text;
---
+++ src/config/weddingData/weddingData.js
@@ -0,0 +1,11 @@
+const weddingData = {
+  boy: 'Samuel',
+  girl: 'Gemma',
+  date: {
+    day: '14',
+    month: '12',
+    year: '1993'
+  }
+}
+
+export default weddingData;
